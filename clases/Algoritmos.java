549f1bc151e35004b6da70f87f9e98c08dce5948
Klithik <santi17461@gmail.com>
2022-12-20 19:34:05 -0300

    cambiada definicion de variables

--- clases/Algoritmos.java
+++ clases/Algoritmos.java
@@ -8,49 +8,49 @@ public class Algoritmos{
     public static void setParticiones(ArrayList<Particion> p){
         particiones = p;
     }
-    public static String ordenamientoStack(Proceso p){
+    public static void ordenamientoStack(Proceso p){
         //BUSCA LA COLA MAS CORTA A LA QUE AGREGAR EL PROCESO
-        int menor_espera=9999999;
+        int menor_espera = -1;
         Particion menor = null;
-        boolean cambio = false;
+
         for (Particion particion : particiones) {
             //SOLO SE TIENEN EN CUENTA PARTICIONES CAPACES
             //DE ALOJAR EL PROCESO
-            if(p.getRecursos()<=particion.getRecursos()){
+            if(p.getRecursos() <= particion.getRecursos()){
                 //SI LA PARTICION NO TIENE UN PROCESO ACTIVO, SIGNIFICA QUE
                 //ESTA INACTIVA, CON LO QUE EL PROCESO ACTUAL SE INSERTA AHI
                 if(particion.getProcesoActivo() == null){
                     particion.setProcesoActivo(p);
-                    return "listo";
+                    return;
                 }
                 //SI LA PARTICION NO ESTA VACIA, SE CONSIDERA EL TIEMPO RESTANTE DEL PROCESO
                 //ACTIVO ACTUAL PARA CONTAR LA LONGITUD TOTAL DE LA COLA
-                int suma = particion.getProcesoActivo().getDuracion();
+                int suma =+ particion.getProcesoActivo().getDuracion();
                 //REVISA SI HAY ELEMENTOS EN LA COLA Y CUAL ES LA ESPERA TOTAL EN ESA COLA
                 if(!particion.getCola().isEmpty()){
                     for (Proceso proceso : particion.getCola()) {
-                        suma=+proceso.getDuracion();
+                        suma =+ proceso.getDuracion();
                     }
                 }
-                //SI LA ENCONTRADA ES LA MAS CORTA HASTA EL MOMENTO GUARDA SU INFORMACION PARA USO FUTURO
-                if(suma<menor_espera){
-                    menor_espera=suma;
-                    menor=particion;
-                    cambio = true;
+                if(menor_espera == -1){
+                    menor_espera = suma;
+                    menor = particion;
+                }else if(suma<menor_espera){
+                    menor_espera = suma;
+                    menor = particion;
                 }
             }
         }
 
         //VERIFICA QUE EN ALGUN MOMENTO SE ENCONTRO UNA PARTICION
         //CAPAZ DE ALOJAR EL PROCESO
-        if(!cambio){
+        if(menor_espera == -1){
             System.out.println("Particion válida no encontrada");
-            return "Ninguna particion válida para el proceso";
-        }else{
-            //SE AGREGA EL PROCESO A LA COLA MAS CORTA ENCONTRADA
-            menor.agregarCola(p);
-            return "agregado";
+            return;
         }
+        //SE AGREGA EL PROCESO A LA COLA MAS CORTA ENCONTRADA
+        menor.agregarCola(p);
+        return;
     }
 
     //ALGORITMO BEST FIT **POR REVISAR**
